62072dec8209015d739088d4700158ee63556ed7
Joseph <aelphaeis@live.com>
2017-01-02 16:00:37 -0500

    Added functionality to server

--- src/main/java/com/crusnikatelier/interceptor/server/InterceptorServer.java
+++ src/main/java/com/crusnikatelier/interceptor/server/InterceptorServer.java
@@ -4,14 +4,20 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.crusnikatelier.interceptor.core.InHandler;
 
 public class InterceptorServer implements Runnable {
 	
 	private static final Logger logger = LoggerFactory.getLogger(InterceptorServer.class);
 	ServerSocket serverSocket;
+	List<Thread> inHandlers;
 	
 	public InterceptorServer(){
 		try {
@@ -19,29 +25,55 @@ public class InterceptorServer implements Runnable {
 		} 
 		catch (IOException e) {
 			String err = "Unable to create server socket";
-			
 			throw new RuntimeException(err, e);
 		}
+		inHandlers = new ArrayList<Thread>();
 	}
 
 	@Override
 	public void run() {
 		try {
-			logger.trace("Waiting to accept client socket");
-			Socket client  = serverSocket.accept();
-		
-			logger.trace("Retrieving input stream");
-			InputStream inStream = client.getInputStream();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			serverSocket.setSoTimeout(500);
+			while (!Thread.interrupted()){
+				try{
+					logger.trace("Waiting to accept client socket");
+					Socket client  = serverSocket.accept();
+					
+					logger.trace("Retrieving input stream");
+					InputStream inStream = client.getInputStream();
+					
+					logger.trace("Setting up input hanlder");
+					InHandler handler = new InHandler(inStream);
+					Thread  handleThread = new Thread(handler);
+					inHandlers.add(handleThread);
+					handleThread.start();
+				}
+				catch(SocketTimeoutException e){
+					//No clients received. Check thread interrupt
+					continue;
+				}
+			}
 		}
-		
+		catch (IOException e) {
+			logger.error("Unable to handle client inputStream", e);
+		}
+		finally{
+			closeSocket();
+		}
 	}
 	
 	public int getPort(){
 		return serverSocket.getLocalPort();
+	}
+	
+	protected void closeSocket(){
+		if(serverSocket != null && !serverSocket.isClosed()){
+			try {
+				serverSocket.close();
+			}
+			catch (IOException e) {
+				logger.error("Unable to close server socket", e);
+			}
+		}
 	}
 }
